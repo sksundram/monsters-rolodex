dd40b8d682965b331799092954cba6bb51497271
Shailesh Kumar Sundram <shailesh.sundram@gmail.com>
2019-11-11 18:11:17 +0530

    Updates

---
+++ precache-manifest.1289b457375f7107057d6fc206288144.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879685dcccff1acc4e064e95e07b6c68",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "747d8cd48d73fc1ab699",
+    "url": "/monsters-rolodex/static/css/main.222d8956.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "747d8cd48d73fc1ab699",
+    "url": "/monsters-rolodex/static/js/main.94fdc75e.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
